aff776799b800803b08a20b3edea812154ffe01c
dnamxtvl <kakitani2000@gmail.com>
2023-11-07 11:23:00 +0700

    delete comment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,6 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap' },
-        // { rel: 'stylesheet', href: './assets/fonts/fontawesome.css' },
-        // { rel: 'stylesheet', href: './assets/fonts/tabler-icons.css' },
-        // { rel: 'stylesheet', href: './assets/fonts/flag-icons.css' }
       ],
     },
   },
